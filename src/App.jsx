e2edcad912319130a6955fe2b74d91ccaa0c63dd
Emily Gorelik <emilygorelik2000@gmail.com>
2024-10-29 12:56:58 -0400

    Can change nametag

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,47 @@ import './styles.css'
 import 'em-button'
 import 'em-nametag'
 import '@novacat35/lit-web-components'
-
+import { useState } from 'react'
 
 function App() {
-  function resetPage() {
-    console.log('reset me');
+  const [firstName, setFirstName] = useState('Emily');
+  const [inputValue, setInputValue] = useState('');
+
+
+  function resetName() {
+    setFirstName('');
+    setInputValue('');
+  }
+
+  function updateName() {
+    setFirstName(inputValue);
+    setInputValue('');
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      updateName();
+    }
   }
 
   return (
-    <> <h1>Vite + React + Novacat + EmGore</h1>
+    <> 
+    <h1>Vite + React + Novacat + EmGore</h1>
     <div className='layoutHor'>
-      <custom-form></custom-form>
       <div>
-        <em-nametag id="nametag" firstName="Emily"></em-nametag>
-        <em-button class="primary" onClick={resetPage}>Reset page</em-button>
+        <div>Please sign in</div>
+        <input type="text" 
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}/>
+        <br />
+        <em-button class="primary" onClick={updateName}>Submit</em-button>
+        <em-button class="error" onClick={resetName}>Reset</em-button>
+      </div>
+      <div>
+        <em-nametag id="nametag" firstName={firstName}></em-nametag>
       </div>
     </div> 
-    
     </>
   )
 }
